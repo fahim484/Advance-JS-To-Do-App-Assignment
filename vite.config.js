474a084c9a732d8e87c93321061320986bea61ce
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-09-02 08:03:19 +0600

    hosting fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: "index.html", // Only the main HTML file is needed
+      input: "index.html", 
     },
   },
 });
